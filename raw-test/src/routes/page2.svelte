135b2c45932a84ed744515dd2562478aab15956d
Maximilian Wunderlich <Maximilian.Wunderlich@werma.com>
2023-06-02 13:49:09 +0200

    Update

--- raw-test/src/routes/page2.svelte
+++
@@ -1,9 +0,0 @@
-<script lang ="ts">
-	let userProp = {
-        name: "Jeff",
-        age: "20",
-        heigt: 5
-    }
-</script>
-
-<h1>test</h1>
--- sveltTestApp/src/routes/+page.svelte
+++ sveltTestApp/src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script lang "ts">
-	let count = 0;
-</script>
-
-<button> Clicked {count} Times</button>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
