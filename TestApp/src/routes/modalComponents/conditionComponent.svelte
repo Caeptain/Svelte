1141d5677db8a28e6f47f568ac0855bc4a030e7d
Maximilian Wunderlich <Maximilian.Wunderlich@werma.com>
2023-06-03 14:32:01 +0200

    Update

---
+++ TestApp/src/routes/modalComponents/conditionComponent.svelte
@@ -0,0 +1,43 @@
+<script>
+	const success = true;
+	const disabled = true;
+	const num = 0;
+
+	export let names = ['Mark', 'Uwe', 'Kling'];
+	const fullNames = [
+		{ name: 'bruce', surname: 'wayn' },
+		{ name: 'clark', surname: 'kent' },
+		{ name: 'prinzess', surname: 'diana' }
+	];
+
+	const shuffleNames = () => {
+		names = names
+			.map((value) => ({ value, sort: Math.random() }))
+			.sort((a, b) => a.sort - b.sort)
+			.map(({ value }) => value);
+	};
+</script>
+
+<div class="conditions">
+	{#each names as item, index (item)}
+		<form>
+			<label for="fistName">{item}</label><br />
+			<input placeholder="Last Name" /><br />
+		</form>
+	{/each}
+
+	<button {disabled} on:click={shuffleNames}>Shuffle!</button>
+
+	<h2 class={success ? 'success' : 'danger'}>{success ? 'Win' : 'Warning!'}</h2>
+	{#if success}<h2 class:success>success</h2> {/if}
+</div>
+
+<style>
+	.danger {
+		color: #ff3e00;
+	}
+
+	.success {
+		color: olive;
+	}
+</style>
---
+++ TestApp/src/routes/modalComponents/costumerComponent.svelte
@@ -0,0 +1,18 @@
+<script>
+	let showModel = true;
+	var firstName = 'fname';
+</script>
+
+{#if showModel}
+	<div class="customer">
+		<h2>Create Customer</h2>
+
+		<label for={firstName}>First name:</label><br />
+		<input type="text" id="fname" name="fname" required /><br />
+		<label for="lname">Last name:</label><br />
+		<input type="text" id="lname" name="lname" required /><br /><br />
+	</div>
+{/if}
+
+<style>
+</style>
