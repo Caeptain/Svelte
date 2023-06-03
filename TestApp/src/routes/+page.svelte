cdd6d7f4d069f7aa760393af569e29f0a67f5e2c
Maximilian Wunderlich <Maximilian.Wunderlich@werma.com>
2023-06-03 14:31:52 +0200

    Update

--- TestApp/src/routes/+page.svelte
+++ TestApp/src/routes/+page.svelte
@@ -1,90 +1,58 @@
 <script>
-	const name = 'Batman';
-	const channel = '<b>CodeRevolution</b>';
-	const hack = `<a href="#" onclick="alert('You have been hacked')"> Win a prize! </a>"`;
-
-	const id = 'heading';
-	const disabled = true;
-
-	const success = true;
-
-	const num = 0;
-
-	let names = ['Mark', 'Uwe', 'Kling'];
-
-	const fullNames = [
-		{ name: 'bruce', surname: 'wayn' },
-		{ name: 'clark', surname: 'kent' },
-		{ name: 'prinzess', surname: 'diana' }
-	];
-	const shuffleNames = () => {
-		names = names
-			.map((value) => ({ value, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ value }) => value);
-	};
+	import Modal from './modal.svelte';
+	const websiteName = 'Plantigo';
 </script>
 
+<svelte:head>
+	<style>
+		body {
+			background: ;
+			color: white;
+			font-size: 1.1em;
+			padding: 0 auto;
+			max-width: 600px;
+			margin: 0 auto;
+		}
+		button {
+			text-align: center;
+			padding: 8px;
+			outline: none;
+			background: white;
+			font-size: 0.8em;
+			margin: 10px;
+		}
+		button:disabled {
+			background: lightgrey;
+			color: grey;
+		}
+		button:focus {
+			background: lightgreen;
+		}
+		h1 {
+			text-transform: uppercase;
+			text-align: center;
+			font-size: 4em;
+			font-weight: 100;
+			color: lightgreen;
+		}
+		h2 {
+			text-transform: uppercase;
+			font-weight: 100;
+			color: yellow;
+		}
+		@media (min-width: 640px) {
+			main {
+				max-width: none;
+			}
+		}
+	</style>
+</svelte:head>
+
 <main>
-	<h1>Hello {name}</h1>
-	<div>{@html channel}</div>
-	<div>{@html hack}</div>
-
-	<h2 {id}>This is a Heading</h2>
-	<button {disabled}>Bind</button>
-
-	<h2 class={success ? 'success' : 'danger'}>{success ? 'Win' : 'Warning!'}</h2>
-	{#if success}<h2 class:success>success</h2> {/if}
-
-	{#if num === 0}
-		<h2>The Number is Zero</h2>
-	{:else if num < 0}
-		<h2>The Number is Negativ</h2>
-	{:else}
-		<h2>The Number is Positv</h2>
-	{/if}
-
-	{#each names as item}
-		<h2>{item}</h2>
-		<input placeholder="Last Name" />
-	{/each}
-	{#each fullNames as item}
-		<h2>{item.surname}</h2>
-	{/each}
-	{#each names as item, index (item)}
-		<h2>{item}</h2>
-		<input placeholder="Last Name" />
-	{/each}
-	<button on:click={shuffleNames}>Shuffle!</button>
+	<title>{websiteName}</title>
+	<h1>{websiteName}</h1>
 </main>
 
-<style>
-	.danger {
-		color: #ff3e00;
-	}
-
-	.success {
-		color: olive;
-	}
-
-	main {
-		text-align: center;
-		padding: 0 auto;
-		max-width: 240px;
-		margin: 0 auto;
-		background-color: #2222;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<body>
+	<Modal />
+</body>
---
+++ TestApp/src/routes/modal.svelte
@@ -0,0 +1,34 @@
+<script>
+	import ConditionComponent from './modalComponents/conditionComponent.svelte';
+	import CustomerView from './modalComponents/costumerComponent.svelte';
+
+	var showCustomer = false;
+	var showConditions = false;
+
+	var showOnlyCustomer = () => {
+		showConditions = false;
+		showCustomer = true;
+	};
+
+	var showOnlyConditions = () => {
+		showConditions = true;
+		showCustomer = false;
+	};
+</script>
+
+<div class="buttons">
+	<button on:click={showOnlyCustomer}>Register Customer</button>
+	<button on:click={showOnlyConditions}>Show Conditions</button>
+</div>
+{#if showCustomer}
+	<CustomerView />
+{/if}
+{#if showConditions}
+	<ConditionComponent />
+{/if}
+
+<style>
+	.buttons {
+		padding: 10px;
+	}
+</style>
